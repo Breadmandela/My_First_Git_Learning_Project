f7bc349b5f2220038ddae660b3596d2b7dc4d1a3
Breadmandela <Nielsen6@mail.usf.edu>
2020-01-23 17:09:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace My_First_Git_Learning_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a user prompt to pause application
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
